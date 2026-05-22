bd30a859baeecee33fb5e7c6e5aebef9ad7bea44
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2026-05-22 07:06:25 +0200

    Add files via upload

---
+++ DisplayingProgramOutput/src/main/java/com/mycompany/displayingprogramoutput/DisplayingProgramOutput.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.displayingprogramoutput;
+
+/**
+ *
+ * @author moka
+ */
+// This Is The Public Class Of The Java Program.
+public class DisplayingProgramOutput {
+
+    // This Is The Main Method , It Is The Entry Point , Or Starting Point Of Your Own Java Computer Program.
+    public static void main(String[] args) {
+        System.out.print("Welcome From Java Language.");
+        System.out.print("I Am Mahmoud Khalid");
+        System.out.println("");
+        System.out.println("Welcome From The Java Language");
+        System.out.println("I Am Mahmoud Khalid");
+        System.out.println("There Are Different Escope Sequences"
+        + " In The Java Language To Format Your Java Computer Program Like:");
+        System.out.println("\n Is Used To Break The Text Into New Line");
+        System.out.println("\t Is Used To Make A Horizontal Tab (Eight Spaces.");
+        System.out.println("I\b Is Used To Break , Or Delete Only Letter From "
+        + "The Sentence.");
+        System.out.println("\" Is Used To Make A Double Quotations");
+        System.out.println("\' Is Used To Make A Single Quotation");
+        System.out.println("\\ Is Used To Make A BackSlash.");
+        System.out.println("There Are Different Example Of The Scope Sequence Like: ");
+        System.out.println("He Said That \"I Am From Egypt Country.\" , But I "
+        + "Lives In \'United States Of American (USA).\'");
+        System.out.println("The File Location Is D:\\\\JavaPrograg\\TryOutput.");
+        System.out.println("The Working Day Is :");
+        System.out.println("Sunday \t Monday \t Thuseday \t Wensday \t Thursday.");
+    }
+}
