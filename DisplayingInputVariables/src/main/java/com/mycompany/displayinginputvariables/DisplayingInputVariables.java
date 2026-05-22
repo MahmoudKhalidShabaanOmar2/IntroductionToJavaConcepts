ef187a4cf972c0a23900c4ee1a5719d850398999
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2026-05-22 13:31:03 +0200

    Add files via upload

---
+++ DisplayingInputVariables/src/main/java/com/mycompany/displayinginputvariables/DisplayingInputVariables.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.displayinginputvariables;
+public class DisplayingInputVariables{
+    public static void main(String[] args){
+    // Declaration Variable With Variable Name , And Variable Data Type...
+    int fristNum , secondNum;
+    // Intialization Variable With Value...
+    fristNum = 12;
+    secondNum = 25;
+    System.out.println(fristNum);
+    System.out.println(secondNum);
+    // Declaration , And Intialization Variable In The Same Step.
+    int thirdNum = 30;
+    System.out.println(thirdNum);
+    // There Are Different Data Types In The Java Language Like:
+    // 1-This Is Used To Make An Small Integer , Or Char (Only One Character).
+    char character = 'A';
+    System.out.println(character);
+    // 2-This Is Used To Make An Integer Number.
+    int num = 30;
+    System.out.println(num);
+    int u = -30;
+    System.out.println(u);
+    // 3-This Is Used To Make A Short Integer Number.
+    short x = 20;
+    System.out.println(x);
+    short a = -20;
+    System.out.println(a);
+    // 4-This Is Used To Make A Long Integer Number.
+    long z = 450000000;
+    System.out.println(z);
+    long s = -450000000;
+    System.out.println(s);
+    // 5-This Is Used To Get One Of Two Values (True (One) , Or False (Zero)).
+    boolean t = true;
+    System.out.println(t);
+    boolean f = false;
+    System.out.println(f);
+    // 6-This Is Used To Make A Floating Point Numbers.
+    float k = 23.2345f;
+    System.out.println(k);
+    float g = -23.2345f;
+    System.out.println(g);
+    // 7-This Is Used To Make A Double Floating Point Precision Numbers.
+    double j = 34.345656789098;
+    System.out.println(j);
+    double q = -34.34567890899;
+    System.out.println(g);
+    }
+}
