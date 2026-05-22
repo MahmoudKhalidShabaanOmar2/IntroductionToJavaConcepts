a3b7006464a0ef3e75e88da8ff0f446ccc45b1ee
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2026-05-22 06:34:12 +0200

    Add files via upload

---
+++ WelcomeProject/src/main/java/com/mycompany/welcomeproject/WelcomeProject.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.welcomeproject;
+
+/**
+ *
+ * @author moka
+ */
+
+// This Is A Puplic Class In Your Own Java Script Code.
+public class WelcomeProject{
+    // This Is The Main Method That Your Own Java Program Starts Execution Your Own Program From This Point.
+    public static void main(String args[]){
+    // This Is Used To Print A Message For The User Of Your Own Java Program.
+    System.out.print("Hello EveryOne! , From Your Own Java Program...");
+    }
+}
