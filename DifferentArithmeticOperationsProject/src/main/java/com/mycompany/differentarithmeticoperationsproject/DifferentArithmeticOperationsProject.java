4af8769fe5d147f77d2aecc3d1c9bdda5dc87396
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2026-06-01 21:32:20 +0200

    Add files via upload

---
+++ DifferentArithmeticOperationsProject/src/main/java/com/mycompany/differentarithmeticoperationsproject/DifferentArithmeticOperationsProject.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.differentarithmeticoperationsproject;
+
+/**
+ *
+ * @author moka
+ */
+// Write A Java Program To Make All Arithmetic Operations On Different Numbers =>
+public class DifferentArithmeticOperationsProject{
+    public static void main(String[] args){
+        System.out.println("Different Arithmetic Operations On Two Numbers Like: ");
+        System.out.println("The Addition Arithmetic Operation On Two Number Is = "+(10+20)); // 30
+        System.out.println("The Addition Arithmetic Operation On Two Number Is = "+(20.45 + 10)); // 30.45
+        System.out.println("The Addition Arithmetic Operation On Two Number Is = "+(10.23 + 5.45)); // 15.68
+        System.out.println("The Subtraction Arithmetic Operation On Two Number Is = "+(20 - 10)); // 10
+        System.out.println("The Subtraction Arithmetic Operation On Two Number Is = "+(10 - 20)); // -10
+        System.out.println("The Subtraction Arithmetic Operation On Two Number Is = "+(10 - -20)); // 30
+        System.out.println("The Subtraction Arithmetic Operation On Two Number Is = "+(-10 -20)); // -30
+        System.out.println("The Subtraction Arithmetic Operation On Two Number Is = "+(-10 - -20)); // 10
+        System.out.println("The Subtraction Arithmetic Operation On Two Number Is = "+(10.234 - 5.456)); // 4.778
+        System.out.println("The Subtraction Arithmetic Operation On Two Number Is = "+(5.56 - 10.23)); // -4.6700
+        System.out.println("The Multiplication Arithmetic Operation On Two Number Is = "+(4 * 5)); // 20
+        System.out.println("The Multiplication Arithmetic Operation On Two Number Is = "+(-4 * 5)); // -20
+        System.out.println("The Multiplication Arithmetic Operation On Two Number Is = "+-4 * -5); // 20
+        System.out.println("The Multiplication Arithmetic Operation On Two Number Is = "+4 * -5); // -20
+        System.out.println("The Multiplication Arithmetic Operation On Two Number Is = "+4.5 * 5.3); // 23.849
+        System.out.println("The Multiplication Arithmetic Operation On Two Number Is = "+-4.5 * 5.3); // -23.849
+        System.out.println("The Multiplication Arithmetic Operation On Two Number Is = "+4.5 * -5.3); // -23.849
+        System.out.println("The Multiplication Arithmetic Operation On Two Number Is = "+-4.5 * -5.3); // 23.849
+        System.out.println("The Division Arithmetic Operation On Two Number Is = "+ 20 / 4); // 5
+        System.out.println("The Division Arithmetic Operation On Two Number Is = "+ 4.0 / 20.0); // 0.2
+        System.out.println("The Division Arithmetic Operation On Two Number Is = "+-20 / 4); // -5
+        System.out.println("The Division Arithmetic Operation On Two Number Is = "+20 / -4); // -5
+        System.out.println("The Division Arithmetic Operation On Two Number Is = "+20.0 / 2.5); // 8
+        System.out.println("The Division Arithmetic Operation On Two Number Is = "+2.5 / 20.0); // 1/8 = 0.125
+        System.out.println("The Division Arithmetic Operation On Two Number Is = "+-20.0 / 2.25); // -8.8888
+        System.out.println("The Division Arithmetic Operation On Two Number Is = "+20.0 / -2.25); // -8.8888
+        System.out.println("The Modulus Arithmetic Operation On Two Number Is = "+20 % 3); // 1
+        System.out.println("The Modulus Arithmetic Operation On Two Number Is = "+20 % 4); // 0
+        System.out.println("The Modulus Arithmetic Operation On Two Number Is = "+4 % 20); // 4
+        System.out.println("The Modulus Arithmetic Operation On Two Number Is = "+4.5 % 2.3); // Run Time Error.
+    }
+}
