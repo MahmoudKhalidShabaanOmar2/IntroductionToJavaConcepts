c0abbf3ea21ff5e770de9730b4fbfc865b774ef9
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2026-06-20 13:45:44 +0200

    Add files via upload

---
+++ DisplayingDifferentPrintingMethods/src/main/java/com/mycompany/displayingdifferentprintingmethods/DisplayingDifferentPrintingMethods.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.displayingdifferentprintingmethods;
+
+/**
+ *
+ * @author moka
+ */
+// There Are Different Methods Of Printing Methods For Different 
+// Users In Your Own Java Computer Programming Codes =>
+public class DisplayingDifferentPrintingMethods{
+    public static void main(String[] args){
+        System.out.print("There Are Different Printing Methods Like : \n");
+        System.out.print("Hello World From The Java Language...\n");
+        System.out.println("Hello World From The Java Language...");
+        System.out.printf("Hello World From The Java Language...");
+    }
+}
