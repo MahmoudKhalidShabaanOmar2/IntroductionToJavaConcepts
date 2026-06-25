c22d31fbdfe5aa29ef087f179425db84380235db
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2026-06-25 22:26:01 +0200

    Add files via upload

---
+++ DisplayingSumOfTwoNumbersFromUserProject/src/main/java/com/mycompany/displayingsumoftwonumbersfromuserproject/DisplayingSumOfTwoNumbersFromUserProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.displayingsumoftwonumbersfromuserproject;
+
+/**
+ *
+ * @author moka
+ */
+public class DisplayingSumOfTwoNumbersFromUserProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
