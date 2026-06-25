cadd110a3978e760153409466eb4e1c316a9e964
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2026-06-25 22:12:27 +0200

    Add files via upload

---
+++ DisplayingDifferentEscapeSequencesInJava/src/main/java/com/mycompany/displayingdifferentescapesequencesinjava/DisplayingDifferentEscapeSequencesInJava.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.displayingdifferentescapesequencesinjava;
+
+/**
+ *
+ * @author moka
+ */
+public class DisplayingDifferentEscapeSequencesInJava{
+    public static void main(String[] args){
+        System.out.println("It Is Break A New"
+        + " Line \nMahmoud Khalid.");
+        System.out.println("It Is Used To Make"
+        + " A Horizontal Tab \t Java Language.");
+        System.out.println("It Is Used To Make "
+        + "A Carrige Return \r 23 Years Old.");
+        System.out.println("It Is Used To Make"
+        + " A Double Quotations \"");
+        System.out.println("It Is Used To Make "
+        + "A Single Quotation \'");
+        System.out.println("It Is Used To Make "
+        + "A BackSlash \\");
+        System.out.println("\nExample Of Different "
+        + "Escape Sequences Like: \n");
+        System.out.println("I Am From \"Egypt\" ,"
+        + " But Lives In \"KSA\"");
+        System.out.println("The Files Located In"
+        + ": D:\\TryProgr\\JavaPrograms");
+        System.out.println("The Working Day Is : ");
+        System.out.println("Sunday\tMonday\tThuseday"
+        + "\tWensday\tThurseday...");
+    }
+}
